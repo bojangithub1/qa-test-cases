9e3968eba74545047876bbe888770a17994c1eb3
bojangithub1 <bojan.github@gmail.com>
2023-12-09 17:10:46 +0100

    Fix test case 1

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebElement;
 
 import java.util.stream.Stream;
 
@@ -25,14 +26,22 @@ public class LoginTest extends UiBaseTest {
   @ParameterizedTest(name = "{0}")
   @MethodSource("loginWithUsernameAndPasswordInputs")
   void testLoginWithUsernameAndPassword(String displayName, String username,
-                                        String password, boolean result) {
+                                        String password,
+                                        boolean successfulLogin) {
     getIndex();
 
     inputUsername(username);
     inputPassword(password);
     login();
 
-    Assertions.assertEquals(result,
+    if (!successfulLogin) {
+      WebElement error = findByDataTest("error");
+      Assertions.assertEquals(
+          "Epic sadface: Username and password do not match any user in this service",
+          error.getText());
+    }
+
+    Assertions.assertEquals(successfulLogin,
         "https://www.saucedemo.com/inventory.html".equals(
             DRIVER.getCurrentUrl()));
   }
